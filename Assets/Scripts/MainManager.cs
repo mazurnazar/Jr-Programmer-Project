372941d4668fd79772f07078f5c0f90907047d5a
mazurnazar <nazarmazur@ukr.net>
2022-03-22 16:08:03 +0200

    optimization

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using System.IO;
 public class MainManager : MonoBehaviour
 {
-    public static MainManager Instance;
+    public static MainManager Instance { get; private set; }
     public Color TeamColor;
     private void Awake()
     {
